bb055d4252397232fd469aee15264f16ce9b9d71
Vojtěch Pejša <vojtechpejsa7@gmail.com>
2020-02-25 14:20:03 +0100

    Don't panic when file does not exist.

--- src/bin.rs
+++ src/bin.rs
@@ -1,9 +1,18 @@
 use mylib::{execute, parse};
 use std::collections::HashMap;
+use std::fs::File;
 use std::io::Read;
+use std::path::Path;
 
 fn usage() {
     eprintln!("program <file>");
+}
+
+fn load_program(file_path: &Path) -> Result<String, std::io::Error> {
+    let mut file = File::open(file_path)?;
+    let mut input = String::new();
+    file.read_to_string(&mut input)?;
+    Ok(input)
 }
 
 fn main() {
@@ -12,14 +21,16 @@ fn main() {
         usage();
         std::process::exit(1)
     });
-    let file_path = std::path::Path::new(&file);
-    let mut file = std::fs::File::open(file_path).unwrap();
-    let mut input = String::new();
-    file.read_to_string(&mut input).unwrap();
-    let program = parse(&input);
-    //println!("{:#?}", program);
-    match execute(&program, &mut HashMap::new()) {
-        Ok(_) => (),
-        Err(e) => eprintln!("Runtime error: {:?}", e),
+    let file_path = Path::new(&file);
+    match load_program(&file_path) {
+        Ok(input) => {
+            let program = parse(&input);
+            //println!("{:#?}", program);
+            match execute(&program, &mut HashMap::new()) {
+                Ok(_) => (),
+                Err(e) => eprintln!("Runtime error: {:?}", e),
+            }
+        }
+        Err(e) => eprintln!("OS error: {:?}", e),
     }
 }
