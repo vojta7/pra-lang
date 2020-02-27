314f7963deb540a5d56408d1c242614b513e1429
Vojtěch Pejša <vojtechpejsa7@gmail.com>
2020-02-27 13:46:31 +0100

    Change how buildin functions work.

--- src/bin.rs
+++ src/bin.rs
@@ -1,8 +1,30 @@
-use mylib::{execute, parse};
+use mylib::ast::{ArgList, VarVal};
+use mylib::{execute, parse, Buildins};
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
+
+fn buildins() -> Buildins<'static> {
+    let mut f: Buildins = HashMap::new();
+    f.insert(
+        "print".to_owned(),
+        Box::from(|args: ArgList| {
+            for arg in args.args {
+                match arg {
+                    VarVal::I32(Some(v)) => print!("{}", v),
+                    VarVal::BOOL(Some(v)) => print!("{}", v),
+                    VarVal::STRING(Some(v)) => print!("{}", v),
+                    VarVal::UNIT => print!("()"),
+                    _ => (),
+                }
+            }
+            println!();
+            VarVal::UNIT
+        }),
+    );
+    f
+}
 
 fn usage() {
     eprintln!("program <file>");
@@ -32,7 +54,7 @@ fn main() {
             match parse(&input) {
                 Ok(program) => {
                     //println!("{:#?}", program);
-                    match execute(&program, &mut HashMap::new()) {
+                    match execute(&program, &mut HashMap::new(), &mut buildins()) {
                         Ok(_) => (),
                         Err(e) => eprintln!("Runtime error: {:#?}", e),
                     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 pub mod ast;
-pub mod buildin;
 mod lexer;
 
 pub use ast::{ArgList, Block, Expr, Function, Opcode, Program, Stmt, VarVal, Variable};
@@ -20,33 +19,36 @@ pub enum RuntimeError {
     WrongNumberOfArguments,
 }
 
+pub type Buildins<'a> = HashMap<String, Box<dyn FnMut(ArgList) -> VarVal + 'a>>;
+
 fn eval(
     expr: &Expr,
     globals: &mut HashMap<String, Variable>,
     program: &Program,
     locals: &mut HashMap<String, Variable>,
+    buildins: &mut Buildins,
 ) -> Result<VarVal, RuntimeError> {
     match expr {
         Expr::Function(name, expr) => {
-            let functions = buildin::buildins();
             let arglist = ArgList {
                 args: expr
                     .iter()
-                    .map(|expr| eval(expr, globals, program, locals))
+                    .map(|expr| eval(expr, globals, program, locals, buildins))
                     .collect::<Result<_, _>>()?,
             };
-            match functions.get(name) {
-                Some(f) => Ok(f(arglist)),
-                None => match program.functions.get(name) {
-                    Some(f) => eval_function(&f, arglist, globals, program),
+            if let Some(f) = buildins.get_mut(name) {
+                Ok(f(arglist))
+            } else {
+                match program.functions.get(name) {
+                    Some(f) => eval_function(&f, arglist, globals, program, buildins),
                     None => Err(RuntimeError::UndefinedFunction(name.clone())),
-                },
+                }
             }
         }
         Expr::Value(n) => Ok(n.clone()),
         Expr::Op(lhs, opc, rhs) => {
-            let l = eval(lhs, globals, program, locals)?;
-            let r = eval(rhs, globals, program, locals)?;
+            let l = eval(lhs, globals, program, locals, buildins)?;
+            let r = eval(rhs, globals, program, locals, buildins)?;
             if let (VarVal::I32(Some(l)), VarVal::I32(Some(r))) = (&l, &r) {
                 match opc {
                     Opcode::Add => Ok(VarVal::I32(Some(l + r))),
@@ -90,13 +92,13 @@ fn eval(
             })
             .map(|v| v.value.clone()),
         Expr::If(if_expr) => {
-            let predicate = eval(&if_expr.condition, globals, program, locals)?;
+            let predicate = eval(&if_expr.condition, globals, program, locals, buildins)?;
             match predicate {
                 VarVal::BOOL(Some(v)) => {
                     if v {
-                        eval_block(&if_expr.if_block, globals, program, locals)
+                        eval_block(&if_expr.if_block, globals, program, locals, buildins)
                     } else if let Some(else_block) = &if_expr.else_block {
-                        eval_block(else_block, globals, program, locals)
+                        eval_block(else_block, globals, program, locals, buildins)
                     } else {
                         Ok(VarVal::UNIT)
                     }
@@ -112,14 +114,15 @@ fn eval_block(
     globals: &mut HashMap<String, Variable>,
     program: &Program,
     locals: &mut HashMap<String, Variable>,
+    buildins: &mut Buildins,
 ) -> Result<VarVal, RuntimeError> {
     for stmt in &block.statements {
         match stmt {
             Stmt::Expr(expr) => {
-                eval(&expr, globals, program, locals)?;
+                eval(&expr, globals, program, locals, buildins)?;
             }
             Stmt::Asgn(id, expr) => {
-                let res = eval(&expr, globals, program, locals)?;
+                let res = eval(&expr, globals, program, locals, buildins)?;
                 locals.insert(
                     id.to_string(),
                     Variable {
@@ -130,7 +133,7 @@ fn eval_block(
             }
         };
     }
-    eval(&block.expr, globals, program, locals)
+    eval(&block.expr, globals, program, locals, buildins)
 }
 
 fn eval_function(
@@ -138,6 +141,7 @@ fn eval_function(
     arglist: ArgList,
     globals: &mut HashMap<String, Variable>,
     program: &Program,
+    buildins: &mut Buildins,
 ) -> Result<VarVal, RuntimeError> {
     let mut locals = HashMap::new();
     if arglist.args.len() != function.arguments.len() {
@@ -148,18 +152,20 @@ fn eval_function(
         var.value = arg_value.clone();
         locals.insert(var.ident.clone(), var);
     }
-    eval_block(&function.block, globals, program, &mut locals)
+    eval_block(&function.block, globals, program, &mut locals, buildins)
 }
 
 pub fn execute(
     program: &Program,
     globals: &mut HashMap<String, Variable>,
+    buildins: &mut Buildins,
 ) -> Result<VarVal, RuntimeError> {
     eval_function(
         &program.functions["main"],
         ArgList { args: Vec::new() },
         globals,
         program,
+        buildins,
     )
 }
 
